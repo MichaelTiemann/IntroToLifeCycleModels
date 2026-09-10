72f5e287e08ae937d857c93237b5321438baceaf
Michael Tiemann <mdtiemann@gmail.com>
2026-09-11 04:36:57 +1200

    Updated vectorization prototype...not working.

--- LifeCycleModel1_VReturnFn.m
+++ LifeCycleModel1_VReturnFn.m
@@ -1,8 +1,8 @@
-function F = LifeCycleModel1_VReturnFn(aprime, a, w, r, sigma, beta)
+function F = LifeCycleModel1_VReturnFn(h, aprime, a, w, sigma, psi, eta)
 
 % 1. Calculate Consumption using element-wise operations (.*, .+, ./)
 % Both 'a' and 'aprime' will be large 1D column vectors.
-c = w + (1 + r) * a - aprime;
+c = w .* h;
 
 % 2. Prepare logical masking for valid consumption
 valid_c = c > 0;
@@ -12,7 +12,7 @@ c_safe = c;
 c_safe(~valid_c) = 1; 
 
 % 3. Vectorized Utility Calculation
-utility = (c_safe.^(1 - sigma)) ./ (1 - sigma);
+utility = (c_safe.^(1-sigma))./(1-sigma) - psi.*(h.^(1+eta))./(1+eta);
 
 % 4. Initialize F and assign valid utilities
 % Use 'like' to maintain gpuArray typing if the inputs are on the GPU
